55465d11654be6228cc578f91ccd6bf4ae2577fd
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2026-02-12 10:45:42 +0100

    E to forward by one frame, need to fix projet shot timecode and slider
    using an estimated time if is set when using get_current_time.
    estimated time is the current time in ms + the duration of one frame.

--- side_menu_widget.py
+++ side_menu_widget.py
@@ -170,18 +170,17 @@ class SideMenuWidget(QDockWidget):
         if not self.vlc_widget.media:
             return
 
-        current_time = self.vlc_widget.player.get_time()
         self.max_time = self.vlc_widget.player.get_length()
 
         if self.display is None:
             return
     
-        # On ajoute une petite marge de la moitié de la durée d'une frame 
-        # pour la comparaison pour compenser l'imprecision de player.get_time()
-        half_frame_duration = (1000.00 / self.vlc_widget.fps) / 2 
+
+        current_time = self.vlc_widget.get_current_time()
+        frame = self.time_manager.m_to_frame(current_time)
 
         for seg in self.display.stock_button:
-            if round(seg["time"]) <= round(current_time) <= round(seg["end"] + half_frame_duration):
+            if seg["frame1"] <= frame <= seg["frame2"]:
                 seg["rect"].setBrush(QBrush(QColor("red")))
                 self.set_position(seg["id"],go=False)
             else:
@@ -196,7 +195,10 @@ class SideMenuWidget(QDockWidget):
         if not self.vlc_widget.media:
             return None
 
-        current_time = self.vlc_widget.player.get_time()
+
+        vlc_time = self.vlc_widget.player.get_time()
+        estimated_time = self.vlc_widget.estimated_time if self.vlc_widget.estimated_time is not None else 0
+        current_time = max(vlc_time, estimated_time)
 
         for i in range(len(self.display.stock_button)):
             btn_data = self.display.stock_button[i]
